7c3a2c9b48d33322328b911f757357ae1caeb811
Utsav kushwaha <94130476+utsav31703@users.noreply.github.com>
2025-08-06 16:44:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import cors from 'cors'
 
 const app =express()
 
-app.use(cors();
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
